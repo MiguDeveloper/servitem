40e38c2ae6b22e10301edb156dab883f6d361b2f
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-11-02 21:11:14 -0500

    spring cloud config y actuator para refresh de nuestros properties

--- src/main/java/pe/tuna/servitem/controllers/ItemController.java
+++ src/main/java/pe/tuna/servitem/controllers/ItemController.java
@@ -1,20 +1,41 @@
 package pe.tuna.servitem.controllers;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.tuna.servitem.models.Item;
 import pe.tuna.servitem.models.Producto;
 import pe.tuna.servitem.service.IItemService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@RefreshScope
 @RestController
 public class ItemController {
+
+    private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${configuracion.texto}")
+    private String textoPropertie;
+
+    // tambien lo podemos pasar como parametro en la funcion
+    @Value("${server.port}")
+    private String puerto;
 
     @Autowired
     @Qualifier("itemServiceFeign")
@@ -24,6 +45,7 @@ public class ItemController {
     public List<Item> listar() {
         return itemService.findAll();
     }
+
 
     @HystrixCommand(fallbackMethod = "metodoAlernativoGetItem")
     @GetMapping("/{id}/cantidad/{cantidad}")
@@ -43,5 +65,18 @@ public class ItemController {
         item.setProducto(producto);
 
         return item;
+    }
+
+    @GetMapping("/obtenerconfig")
+    public ResponseEntity<?> obtenerConfig(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("texto", textoPropertie);
+        response.put("puerto", puerto);
+        log.info(textoPropertie);
+        if (environment.getActiveProfiles().length>0 && environment.getActiveProfiles()[0].equals("dev")){
+            response.put("autor.nombre", environment.getProperty("configuracion.autor.nombre"));
+            response.put("autor.email", environment.getProperty("configuracion.autor.email"));
+        }
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 }
